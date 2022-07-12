22a1f9150d8fd5adca97bbfcca91ee258fa61048
Christoffer <chros928@student.liu.se>
2022-07-12 15:13:53 +0200

    Initial Commit

---
+++ klader-efter-vader-ios/klader_efter_vader_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  klader_efter_vader_iosApp.swift
+//  klader-efter-vader-ios
+//
+//  Created by Christoffer on 2022-07-12.
+//
+
+import SwiftUI
+
+@main
+struct klader_efter_vader_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
