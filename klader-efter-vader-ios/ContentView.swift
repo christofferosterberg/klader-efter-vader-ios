0afe49833a14d9a5125b558d096eaf8678367345
Christoffer <chros928@student.liu.se>
2022-07-22 13:10:46 +0200

    tried to make API call

--- klader-efter-vader-ios/ContentView.swift
+++ klader-efter-vader-ios/ContentView.swift
@@ -7,7 +7,21 @@
 
 import SwiftUI
 
+struct City: Codable {
+    var id: Int
+    var latitude, longitude: Double
+    var name: String
+}
+
+struct Cities: Codable {
+    var cities: [City]
+}
+
 struct ContentView: View {
+    @State private var cities = [City]()
+    
+//    @State private var joke: String = ""
+//
     var body: some View {
         ScrollView{
             VStack(alignment: .center, spacing: 10.0) {
@@ -17,19 +31,19 @@ struct ContentView: View {
                 .padding()
                 .frame(width: 390.0)
                 .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(red: 0.788, green: 0.949, blue: 1.0)/*@END_MENU_TOKEN@*/)
-                
+
                 VStack(alignment: .center){
                     Text("Välkommen till Kläder Efter Väder!")
                         .font(.title2)
                         .fontWeight(.semibold)
                         .multilineTextAlignment(.center)
-                    Text("Kläder efter väder finns här för att hjälpa dig med det svåra i vardagen, att välja kläder! Kanske ser det ut att vara strålande sol just nu men ska bli molnigt och blåsigt ikväll. Kanske är det molnigt nu men ska spöregna om några timmar. Hur det än blir så hjälper vi dig med klädvalet!")
+                    Text("Kläder efter väder finns här för att hjälpa dig med det svåra i vardagen, att välja kläder! Kanske ser det ut att vara strålande sol just nu men ska bli molnigt och blåsigt ikväll. Kanske är det molnigt nu men ska spöregna om några t viimmar. Hur det än blir så hjälper vi dig med klädvalet!")
                         .padding(.vertical)
                         .frame(maxWidth: .infinity)
-                    
+
                 }
                 .padding(.horizontal)
-                
+
                 VStack(alignment: .leading){
                     Text("Vilken stad bor du i?")
                         .font(.title3)
@@ -39,7 +53,7 @@ struct ContentView: View {
                         .border(.gray)
                 }
                 .padding(.horizontal)
-                
+
                 VStack(alignment: .center){
                     Button() {
                     } label: {
@@ -50,7 +64,7 @@ struct ContentView: View {
                             .foregroundColor(/*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/)
                             .cornerRadius(40)
                     }
-                    
+
                     Button() {
                     } label: {
                         Text("Ta reda på om du behöver ta pollenmedicin idag!")
@@ -60,7 +74,7 @@ struct ContentView: View {
                             .foregroundColor(/*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/)
                             .cornerRadius(40)
                     }
-                    
+
                     Button() {
                     }label:{
                         Text("Ta reda på hur stark solkyddsfaktor du behöver ta idag!")
@@ -72,7 +86,7 @@ struct ContentView: View {
                     }
                 }
                 .padding(.all)
-                
+
                 VStack(alignment: .leading){
                     Text("Vädret just nu ")
                         .fontWeight(.semibold)
@@ -103,19 +117,89 @@ struct ContentView: View {
                         .multilineTextAlignment(.leading)
                 }
                 .padding(.horizontal)
-                
+
                 Image("sweden-map")
-                
-                
+
+
             } // Outer VStack
             .padding([.leading, .bottom, .trailing])
             .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.white/*@END_MENU_TOKEN@*/)
-            
+
         } // ScrollView
         .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(red: 0.788, green: 0.949, blue: 1.0)/*@END_MENU_TOKEN@*/)
     }
 }
+        
+        
+//        Text(joke)
+//                Button {
+//                    Task {
+//                        let (data, _) = try await URLSession.shared.data(from: URL(string:"https://api.chucknorris.io/jokes/random")!)
+//                        let decodedResponse = try? JSONDecoder().decode(Joke.self, from: data)
+//                        joke = decodedResponse?.value ?? ""
+//                    }
+//                } label: {
+//                    Text("Fetch Joke")
+//                }
+//    }
+//}
+        
 
+//        List(cities, id: \.cityId) { item in
+//            VStack(alignment: .leading) {
+//                let latitude = "\(item.latitude)"
+//                let longitude = "\(item.longitude)"
+//                Text(item.cityName)
+//                    .font(.headline)
+//                Text("lat: " + latitude + " long: " + longitude)
+//            }
+//        }
+//        .task {
+//            await loadWeather()
+//        }
+        
+//    } // body
+    
+//    func loadWeather() async {
+//        guard let url = URL(string: "https://klader-efter-vader.herokuapp.com/cities") else {
+//            print("Felaktigt anrop")
+//            return
+//        }
+//
+//        let request = URLRequest(url: url)
+//
+//        URLSession.shared.dataTask(with: request) { data, response, error in
+//            if let data = data {
+//                print(data)
+//                if let decodedResponse = try? JSONDecoder().decode(Cities.self, from: data) {
+//                    // we have good data – go back to the main thread
+//                    DispatchQueue.main.async {
+//                        // update our UI
+//                        self.cities = decodedResponse.cities
+//                    }
+//
+//                    // everything is good, so we can exit
+//                    return
+//                }
+//            }
+//
+//            // if we're still here it means there was a problem
+//            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+//        }.resume()
+//
+//        //        do {
+//        //            let (data, _) = try await URLSession.shared.dataTask(with: url)
+//        //
+//        //            if let decodedCities = try? JSONDecoder().decode(Cities.self, from: data) {
+//        //                cities = decodedCities.cities
+//        //                print(cities)
+//        //            }
+//        //        } catch {
+//        //            print("Felaktig data")
+//        //        }
+//    }
+//}
+//
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
@@ -124,3 +208,7 @@ struct ContentView_Previews: PreviewProvider {
         }
     }
 }
+//
+//struct Joke: Codable {
+//    let value: String
+//}
